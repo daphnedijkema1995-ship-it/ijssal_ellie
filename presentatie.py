7201dca0baf49a6cc3483247817c3a520970c4b2
Daphne Dijkema <daphnedijkema1995@gmai.com>
2026-04-10 14:15:00 +0200

    boelhouding en hoofd_programma toegevoegd

---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(mijn_dict, totaal):
+    for item, bedrag in mijn_dict.items():
+        print (f"{item} : {bedrag} euro")
+
+
+    print("====================================")
+    print(f"totaal : {totaal} euro")
